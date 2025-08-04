b88108803a4105256511790152b18e83b74260e0
Dekoder-py <dekod3r.py@gmail.com>
2025-08-04 13:39:20 +1200

    add random gen

--- main.cpp
+++ main.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
+#include <random>
 
 using std::cout, std::cin, std::endl;
 
-int main(){
-  int secretNumber = 7;
+int main() {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dist(1, 100);
+
+  int secretNumber = dist(gen);
+
   int guess;
 
-  do {
-  cout << "Enter your guess: ";
-  cin >> guess;
-  } while (guess != secretNumber);
+  cout << secretNumber << endl; // DEBUGGING - DELETE LATER
+
+  cout << "Guess the number! It's between 1 and 100." << endl;
+
+  while (true) {
+    cout << "Enter your guess: ";
+    cin >> guess;
+
+    if (guess == secretNumber)
+      break;
+    else {
+      if (guess > secretNumber)
+        cout << "The number is smaller than " << guess << endl;
+      else
+        cout << "The number is bigger than " << guess << endl;
+    }
+  }
 
   cout << "You guessed it! The number is " << secretNumber << endl;
 }
